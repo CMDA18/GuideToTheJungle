b0ef51b4de09882d3ca861e097e83ed8e702e9d4
Luisa <luisasbraga@gmail.com>
2018-05-17 10:13:42 +0200

    added navigation, basic pages and styles

--- config/testSetup.js
+++ config/testSetup.js
@@ -1,25 +1,25 @@
-var enzyme = require('enzyme')
-var Adapter = require('enzyme-adapter-react-16')
-const { jsdom } = require('jsdom')
+// var enzyme = require('enzyme')
+// var Adapter = require('enzyme-adapter-react-16')
+// const { jsdom } = require('jsdom')
 
-enzyme.configure({ adapter: new Adapter() })
+// enzyme.configure({ adapter: new Adapter() })
 
-global.document = jsdom('')
-global.window = document.defaultView
-global.navigator = {
-  userAgent: 'node.js'
-}
-global.requestAnimationFrame = (callback) => {
-  setTimeout(callback, 0)
-}
+// global.document = jsdom('')
+// global.window = document.defaultView
+// global.navigator = {
+//   userAgent: 'node.js'
+// }
+// global.requestAnimationFrame = (callback) => {
+//   setTimeout(callback, 0)
+// }
 
-function copyProps (src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce((result, prop) => ({
-      ...result,
-      [prop]: Object.getOwnPropertyDescriptor(src, prop)
-    }), {});
-  Object.defineProperties(target, props)
-}
-copyProps(document.defaultView, global)
+// function copyProps (src, target) {
+//   const props = Object.getOwnPropertyNames(src)
+//     .filter(prop => typeof target[prop] === 'undefined')
+//     .reduce((result, prop) => ({
+//       ...result,
+//       [prop]: Object.getOwnPropertyDescriptor(src, prop)
+//     }), {});
+//   Object.defineProperties(target, props)
+// }
+// copyProps(document.defaultView, global)
